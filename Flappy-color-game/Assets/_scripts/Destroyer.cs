42d8c7d5f0ac0cf64e0567bc365c9e1a5cd9c3a2
ahmedthebest <ahmedthebest31@gmail.com>
2019-12-27 13:00:00 +0200

    add the color switch base project
    add the color switch base project

---
+++ Flappy-color-game/Assets/_scripts/Destroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        // maby we shoud increase the score or do some thing
+    }
+
+} // end class
---
+++ Flappy-color-game/Assets/_scripts/Spowner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Spowner : MonoBehaviour {
+
+
+    [SerializeField] GameObject[] brefabs;
+    float waiteTimer = 3f;
+
+
+    private void Start()
+    {
+        InvokeRepeating("Spowne", 1, waiteTimer)
+    }
+
+    void Spowne()
+    {
+        int index = Random.Range(0, brefabs.Length);
+        Instantiate(brefabs[index], transform.position, Quaternion.identity);
+    }
+
+ 
+} // end class
